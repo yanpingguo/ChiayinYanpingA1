348108fa65a06d65a02ca68ed2e3436bcc13763b
郭艳萍 <larssamg@gmail.com>
2024-05-14 18:01:09 -0400

    add comment
    Signed-off-by: 郭艳萍 <larssamg@gmail.com>

--- calculator.cs
+++ calculator.cs
@@ -23,18 +23,22 @@ namespace ChiayinYanpingA1
 
         private void ButtonDigit_Click(object sender, EventArgs e)
         {
+            //if isNewNumber is true clear the textbox & equation label, set isNewNumber to false
             if (isNewNumber)
             {
                 LabelDisplay.Text = "";
                 isNewNumber = false;
                 caTextBox.Clear();
             }
+
+            //get digit and one decimal point
             var buttonText = ((Button)sender).Text;
             if (buttonText == "." && caTextBox.Text.Contains("."))
             {
                 return;
             }
 
+            //combine digit and decimal point
             caTextBox.Text += buttonText;
             LabelDisplay.Text += buttonText;
         }
@@ -43,13 +47,12 @@ namespace ChiayinYanpingA1
         {
             caTextBox.Clear();
             pendingOperation = "";
-            x = 0;
             LabelDisplay.Text = "";
-            isNewNumber = true;
         }
 
         private void ButtonOpenration_Click(object sender, EventArgs e)
         {
+            //stored x value and get pendingOperation
             x = double.Parse(caTextBox.Text);
             pendingOperation = ((Button)sender).Text;
             LabelDisplay.Text = x + pendingOperation;
@@ -59,8 +62,11 @@ namespace ChiayinYanpingA1
 
         private void ButtonEqual_Click(object sender, EventArgs e)
         {
+            //get another value y
             double y = double.Parse(caTextBox.Text);
             double result = 0;
+
+            //calculate according to different operators
             switch (pendingOperation)
             {
                 case "+":
